4b74b4323a012f6480525e4c476a0eb3b116f51e
Captain-Abdu <69305151+Captain-Abdu@users.noreply.github.com>
2020-09-10 12:51:00 +0530

    Add files via upload

---
+++ includes/brands-slider.php
@@ -0,0 +1,91 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ADIDAS.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/GUCCI.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/BURBERRY.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/VANS.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/PRADA.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/PUMA.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/REEBOK.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+                <div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/MICHAEL KORS.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/NEWBALANCE.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/CHANEL.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/FILA.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/TOMMY.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/SKECHERS.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
---
+++ includes/footer.php
@@ -0,0 +1,110 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3><span style="color:red">e</span><span style="color:blue">b</span><span style="color:yellow">u</span><span style="color:lawngreen">y</span><span style="color:black">.in</span></h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us">WHERE FASHION COMES TO LIFE!</p>
+    
+        <div class="social-icons">
+            
+        <a href="http://facebook.com/transvelo" class='active'><i class="icon fa fa-facebook"></i></a>
+        <a href="#"><i class="icon fa fa-twitter"></i></a>
+        <a href="#"><i class="icon fa fa-linkedin"></i></a>
+        <a href="#"><i class="icon fa fa-rss"></i></a>
+        <a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening time</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Monday-Friday:</td><td class="pull-right">08:00 To 20:00</td></tr>
+					<tr><td>Saturday:</td><td class="pull-right">09:00 To 21:00</td></tr>
+					<tr><td>Sunday:</td><td class="pull-right">10.00 To 17:00</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">Information</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Bangalore,karnataka</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(+91)1234567890<br>(+91)098654321</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    
+                    <span><a href="#">abduthegreat@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
